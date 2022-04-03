a3433ef41cdc662ce84a8cbe70ca24ffd984eafb
CamilleRampillon <camille.rampillon@gmail.com>
2022-04-03 10:41:52 +0200

    routes + navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navigation from '../components/Navigation';
 
 const Home = () => {
     return (
         <div>
-            home sweet home
+            <Navigation />
         </div>
     );
 };
