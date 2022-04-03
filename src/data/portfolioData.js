e8f3211131b5c26a6d528ed538ef784fc5c9f483
CamilleRampillon <camille.rampillon@gmail.com>
2022-04-03 16:56:19 +0200

    pages: portfolio

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,56 +1,56 @@
 export const portfolioData = [
   {
     id: 1,
-    name: 'ipsum.com',
-    languages: ['javascript','css'],
-    languagesIcons: ['fab fa-js','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project1.JPG'
+    name: 'Kasa',
+    languages: ['react','css','html'],
+    languagesIcons: ['fab fa-react','fab fa-css3-alt'],
+    source: 'https://github.com/CamilleRampillon/CamilleRampillon_11_20122021',
+    info: 'Projet d\'études: création d\'une plateforme de locations d’appartements entre particuliers.',
+    picture: './media/kasa.png'
   },
   {
     id: 2,
-    name: 'lorem.fr',
-    languages: ['javascript','react'],
-    languagesIcons: ['fab fa-js','fab fa-react'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project2.JPG'
+    name: 'Les petits plats',
+    languages: ['javascript','css','html'],
+    languagesIcons: ['fab fa-js','fab fa-css3-alt'],
+    source: 'https://github.com/CamilleRampillon/CamilleRampillon_7_27082021',
+    info: 'Projet d\'études: création d\'un site de recettes de cuisine',
+    picture: './media/petitsPlats.png'
   },
   {
     id: 3,
-    name: 'dolorset.fr',
-    languages: ['javascript','php','css'],
-    languagesIcons: ['fab fa-js','fab fa-php','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project3.JPG'
+    name: 'FishEye',
+    languages: ['javascript','sass','html'],
+    languagesIcons: ['fab fa-js','fab fa-sass','html'],
+    source: 'https://github.com/CamilleRampillon/CamilleRampillon_6_16072021',
+    info: 'Projet d\'études: création d\'un un site web qui permet aux photographes indépendants de présenter leurs meilleurs travaux.',
+    picture: './media/fisheye.png'
   },
   {
     id: 4,
-    name: 'laboriosam.fr',
-    languages: ['javascript','php','react'],
-    languagesIcons: ['fab fa-js','fab fa-php','fab fa-react'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project4.JPG'
+    name: 'OhMyFood',
+    languages: ['sass','html'],
+    languagesIcons: ['fab fa-sass'],
+    source: 'https://github.com/CamilleRampillon/CamilleRampillon_3_22042021',
+    info: 'Projet d\'études: création d\'un site 100% mobile qui répertorie les menus de restaurants gastronomiques.',
+    picture: './media/ohMyFood.png'
   },
   {
     id: 5,
-    name: 'repudiante.com',
-    languages: ['javascript','css'],
-    languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project5.JPG'
+    name: 'Reservia',
+    languages: ['sass','html'],
+    languagesIcons: ['fab fa-sass'],
+    source: 'https://github.com/CamilleRampillon/CamilleRampillon_2_22032021',
+    info: 'Projet d\'études: Intégration des maquettes pour un site de planification de vacances.',
+    picture: './media/reservia.png'
   },
   {
     id: 6,
-    name: 'test.io',
-    languages: ['javascript','react', 'css'],
-    languagesIcons: ['fab fa-js','fab fa-react','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project6.JPG'
+    name: 'SportSee',
+    languages: ['react', 'css','html'],
+    languagesIcons: ['fab fa-react','fab fa-css3-alt'],
+    source: 'https://github.com/CamilleRampillon/CamilleRampillon_12_13012022',
+    info: 'Projet d\'études: Projet d\'études: création d\'une page va notamment permettre à l\’utilisateur de suivre le nombre de sessions réalisées ainsi que le nombre de calories brûlées.',
+    picture: './media/sportsee.png'
   },
 ]
